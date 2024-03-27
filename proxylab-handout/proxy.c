2dd9bbc76bb87cda72facd06e79cf4e2ae49a774
maomao <2471680960@qq.com>
2024-03-27 18:12:04 +0800

    cacheUrl

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -3,6 +3,8 @@
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
+//cache line
+#define CACHELINE 10
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
@@ -20,6 +22,27 @@ void build_header(rio_t *rp, char* serverbuf, URL* headers);
 //part2
 void *thread(void *vargp);
 
+//part3
+typedef struct {
+	char buf[MAX_OBJECT_SIZE];
+	char url[MAXLINE];
+	int size;
+	int valid;
+	int time;
+} cacheLine;
+typedef struct{
+	cacheLine line[CACHELINE];
+	int readcnt, curtime;
+	sem_t mutex, writer;
+} cache_t;
+
+cache_t cache;
+
+void cache_init();
+int getCacheIndex(char *url);
+void cacheWrite(char* buf, char *url, int size);
+
+
 int main(int argc, char **argv)
 {
 	int listenfd;
@@ -31,7 +54,9 @@ int main(int argc, char **argv)
 	sem_t mutex;
 	int *connfdp;
 	pthread_t tid;
-	Sem_init(&mutex, 0, 1);
+	cache_init();
+	
+	
 	/* Check command line args */
 	if(argc != 2){
 		fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -42,9 +67,9 @@ int main(int argc, char **argv)
 	while(1) {
 		clientlen = sizeof(clientaddr);
 		connfdp = Malloc(sizeof(int));
-		P(&mutex);
+		//P(&mutex);
 		*connfdp = Accept(listenfd, (SA*) &clientaddr, &clientlen);
-		V(&mutex);
+		//V(&mutex);
 		Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
 		printf("Accepted connection from (%s, %s)\n", hostname, port); 
 		Pthread_create(&tid, NULL, thread, connfdp);
@@ -62,7 +87,7 @@ int main(int argc, char **argv)
 /* $begin doit */
 void doit(int fd) {
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char buf2server[MAXLINE];
+    char buf2server[MAXLINE], uri1[MAXLINE];
     URL header;
 	rio_t rio, rio2server;
 
@@ -76,6 +101,26 @@ void doit(int fd) {
         printf("Proxy does not implement this method");
         return;
     } 
+    strcpy(uri1, uri); // uri will be changed in parse_uri, so it needs to be preserved
+    int idx = getCacheIndex(uri);
+    if(idx != -1){
+    	P(&cache.mutex);
+    	cache.readcnt++;
+    	if(cache.readcnt == 1){
+    		P(&cache.writer);
+    	}
+    	V(&cache.mutex);
+    	Rio_writen(fd, cache.line[idx].buf, cache.line[idx].size);
+    	P(&cache.mutex);
+    	cache.readcnt--;
+    	if(cache.readcnt == 0){
+    		P(&cache.writer);
+    	}
+    	V(&cache.mutex);
+    	printf("Cache refresh!\n");
+    	return ;
+    }
+    
     parse_uri(uri, &header);
     build_header(&rio, buf2server, &header);
     
@@ -84,12 +129,20 @@ void doit(int fd) {
     Rio_readinitb(&rio2server, serverfd); // p600
     Rio_writen(serverfd, buf2server, strlen(buf2server));
     ssize_t n;
+    
+    int cacheSize = 0;
+    memset(buf, 0, sizeof(buf));
     while((n = Rio_readlineb(&rio2server, buf2server, MAXLINE)) != 0){
     	printf("Proxy received %ld bytes from server, and send to client\n", n);
     	//Rio_writen(fd, buf2server, strlen(buf2server) ); 
     	Rio_writen(fd, buf2server, n ); 
+    	
+    	cacheSize+= n;
+    	strcat(buf, buf2server);
+    	//sprintf(buf, "%s%s", buf, buf2server);
     	// p602, when next write to buf, last write will not be discard, so the return of Rio_readlineb is key
     }
+    cacheWrite(buf, uri1, cacheSize);
     Close(serverfd);
 }
 /*
@@ -166,7 +219,59 @@ void *thread(void *vargp){
 	return NULL;
 }
 
+/* init cache */
+void cache_init(){
+	cache.readcnt = 0;
+	cache.curtime = 0;
+	Sem_init(&cache.mutex, 0, 1);
+	Sem_init(&cache.writer, 0, 1);
+	for(int i = 0; i < CACHELINE; ++i){
+		cache.line[i].valid = 0;
+		cache.line[i].time = 0;
+		cache.line[i].size = 0;
+	}
+}
 
+/* return cacheindex if the url is in the cache 
+ * return -1 if the url is not in the cache
+ */
+int getCacheIndex(char *url){
+	int returnv = -1;
+	for(int i = 0; i < CACHELINE; ++i){
+		if(cache.line[i].valid && !strcmp(cache.line[i].url, url)){
+			returnv = i;
+			break;
+		}
+	}
+	return returnv;
+}
+
+void cacheWrite(char *buf, char * url, int size){
+	if(size > MAX_OBJECT_SIZE) return;
+	int idx = -1;
+	for(int i = 0; i < CACHELINE; ++i){
+		if(cache.line[i].valid == 0){
+			idx = i;
+			break;
+		}
+	}
+	if(idx == -1){
+		int maxtime = 0;
+		for(int i =0; i < CACHELINE; ++i){
+			if(cache.line[i].valid && (cache.curtime - cache.line[i].time) > maxtime){
+				idx = i;
+				maxtime = cache.curtime - cache.line[i].time;
+			}
+		}
+	}
+	P(&cache.writer);
+	strcpy(cache.line[idx].buf, buf);
+	strcpy(cache.line[idx].url, url);
+	cache.line[idx].size = size;
+	cache.line[idx].time = ++cache.curtime;
+	cache.line[idx].valid = 1;
+	V(&cache.writer);
+}
 
 
 
